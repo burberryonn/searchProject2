6afb8a9ee345ddc124e89ebe78f52dcf40123fbb
burberryonn <swaronworld2@mail.ru>
2024-07-11 15:46:54 +0300

    english request done

--- client/src/page/news/NewsCard.jsx
+++ client/src/page/news/NewsCard.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import "./NewsCard.css";
+
 function NewsCard({ news }) {
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleExpand = () => {
+    setExpanded(!expanded);
+  };
+
   return (
-    <div className="news-item">
+    <div className={`news-item ${expanded ? "expanded" : ""}`}>
       <h2>{news.title}</h2>
       {news.image && <img src={news.image} alt={news.title} />}
-      <p>{news.text}</p>
+      <p className="news-text">{news.text}</p>
       <p>
         <strong>Дата публикации:</strong>{" "}
         {new Date(news.publish_date).toLocaleDateString()}
       </p>
       <p>Оригинал статьи</p>
       <a href={news.url}>{news.url}</a>
+      <button onClick={toggleExpand}>
+        {expanded ? "Свернуть" : "Подробнее"}
+      </button>
     </div>
   );
 }
