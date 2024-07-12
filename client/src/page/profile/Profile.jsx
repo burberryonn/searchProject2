b9da0fd26c3b759716617f77852431447c5ce6c6
Вероника Авдеева <veron.avdeeva2015@yandex.ru>
2024-07-12 15:10:07 +0300

    createRequest

--- client/src/page/profile/Profile.jsx
+++ client/src/page/profile/Profile.jsx
@@ -1,13 +1,45 @@
-import React from'react';
-import {useUser } from '../../context/userContext';
+import React, { useEffect, useState } from "react";
+import { useUser } from "../../context/userContext";
+import axios from "axios";
 function Profile() {
-  const {user} = useUser()
-  
+  const { user } = useUser();
+  const [table, useTable] = useState([]);
+  const historyTable = async () => {
+    const field = await axios.get("/api/requestHistory");
+    useTable(field.data);
+  };
+  const deleteRequest = async (id) => {
+    const field = await axios.delete(`/api/requestHistory/${id}`);
+    if(field.data.message==="success"){
+      console.log(id, table);
+      useTable((prev)=>prev.filter((el)=> el.id !== id))
+    }
+  };
+  useEffect(() => {
+    historyTable();
+  }, []);
   return (
+    <>
       <div>
         <h1>{user?.name}</h1>
         <h1>{user?.email}</h1>
       </div>
+      <div>
+        {table.map((el) => {
+          return (
+            <div key={el.id}>
+              <div>
+                {el.goodRequest} {el.badRequest}
+              </div>
+              <button onClick={()=>{
+                deleteRequest(el.id)
+
+              }}>Delete </button>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
