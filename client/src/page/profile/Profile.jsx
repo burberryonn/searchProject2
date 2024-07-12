d8b6154fa01c089d01ef0cf34de5b4cde4ba1c85
burberryonn <swaronworld2@mail.ru>
2024-07-12 12:34:53 +0300

    feature create profile finish

--- client/src/page/profile/Profile.jsx
+++ client/src/page/profile/Profile.jsx
@@ -5,8 +5,8 @@ function Profile() {
   
   return (
       <div>
-        <h1>{user.name}</h1>
-        <h1>{user.email}</h1>
+        <h1>{user?.name}</h1>
+        <h1>{user?.email}</h1>
       </div>
   );
 }
